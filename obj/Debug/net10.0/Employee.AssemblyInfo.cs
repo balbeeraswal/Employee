92ec545bf2805a644fe8061c49cbe48d4aa1ddb1
Balbeer Aswal <balbeeraswal@gmail.com>
2026-05-10 12:36:49 +0530

    Committed again

--- obj/Debug/net10.0/Employee.AssemblyInfo.cs
+++ obj/Debug/net10.0/Employee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709dbfe4615500bdf9c9d59f174d75c615c2f87f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67daee54239b442bb080552677c4bf50227bcac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
